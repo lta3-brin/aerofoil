48781de0e4912a701db23ce6c05477b06d5d8f37
Arizal Akbar Zikri <akbarpn136@gmail.com>
2023-12-14 10:23:29 +0700

    Membuat command awal

--- aerofoil/main.py
+++ aerofoil/main.py
@@ -1,2 +1,18 @@
-def run():
-  print("aerofoil")
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def shoot():
+  """
+  Shoot the portal gun
+  """
+  typer.echo("Shooting portal gun")
+
+
+@app.command()
+def load():
+  """
+  Load the portal gun
+  """
+  typer.echo("Loading portal gun")
