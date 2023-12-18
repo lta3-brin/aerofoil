607cfa96825f465e4b7068005417319ba5111720
akbarpn136 <akbarpn136@gmail.com>
2023-12-18 07:50:59 +0700

    Menambahkan info app

--- aerofoil/main.py
+++ aerofoil/main.py
@@ -1,18 +1,19 @@
 import typer
 
-app = typer.Typer()
+app = typer.Typer(help="Aplikasi CLI untuk keperluan pelatihan model CNN.")
+
 
 @app.command()
 def shoot():
-  """
-  Shoot the portal gun
-  """
-  typer.echo("Shooting portal gun")
+    """
+    Shoot the portal gun
+    """
+    typer.echo("Shooting portal gun")
 
 
 @app.command()
 def load():
-  """
-  Load the portal gun
-  """
-  typer.echo("Loading portal gun")
+    """
+    Load the portal gun
+    """
+    typer.echo("Loading portal gun")
