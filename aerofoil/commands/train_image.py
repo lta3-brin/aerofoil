7cb053357c0fe1f5ededa0bd7e9c20c944df41fb
akbarpn136 <akbarpn136@gmail.com>
2023-12-19 16:18:09 +0700

    Koreksi penggunaan variabel

--- aerofoil/commands/train_image.py
+++ aerofoil/commands/train_image.py
@@ -6,8 +6,6 @@ import tensorflow_datasets as tfds
 
 from aerofoil.helpers.image import normalize_img
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
 
 def trainimg(
     batchsize: Annotated[
@@ -15,36 +13,37 @@ def trainimg(
     ] = 100,
     showcount: Annotated[bool, typer.Option(help="Tampilkan jumlah dataset?")] = False,
     imgshowtrain: Annotated[
-        bool, typer.Option(help="Tampilkan gambar sampel train?")
+        bool, typer.Option(help="Tampilkan gambar sampel untuk latihan?")
     ] = False,
-    imgshowtest: Annotated[
-        bool, typer.Option(help="Tampilkan gambar sampel train?")
+    imgshowvalid: Annotated[
+        bool, typer.Option(help="Tampilkan gambar sampel untuk validasi?")
     ] = False,
 ):
-    (dstrain, dstest), info = tfds.load(
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    (dstrain, dsvalid), info = tfds.load(
         "aerofoil_datasets",
-        split=["train", "test"],
+        split=["train", "valid"],
         batch_size=batchsize,
         with_info=True,
         as_supervised=True,
         shuffle_files=True,
     )
 
-    if showcount:
-        print(f"Jumlah datasets (Train) = {len(dstrain)}")
-        print(f"Jumlah datasets (Test) = {len(dstest)}")
-
-    if imgshowtrain:
-        tfds.show_examples(dstrain, info, is_batched=True)
-
-    if imgshowtest:
-        tfds.show_examples(dstest, info, is_batched=True)
-
     dstrain = dstrain.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     dstrain = dstrain.cache()
     dstrain = dstrain.shuffle(info.splits["train"].num_examples)
     dstrain = dstrain.prefetch(tf.data.AUTOTUNE)
 
-    dstest = dstest.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    dstest = dstest.cache()
-    dstest = dstest.prefetch(tf.data.AUTOTUNE)
+    dsvalid = dsvalid.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    dsvalid = dsvalid.cache()
+    dsvalid = dsvalid.prefetch(tf.data.AUTOTUNE)
+
+    if showcount:
+        print(f"Jumlah datasets (Train) = {len(dstrain)}")
+        print(f"Jumlah datasets (Test) = {len(dsvalid)}")
+
+    if imgshowtrain:
+        tfds.show_examples(dstrain, info, is_batched=True)
+
+    if imgshowvalid:
+        tfds.show_examples(dsvalid, info, is_batched=True)
