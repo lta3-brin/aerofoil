b3542d42640049aab14fcdb454fecfb112a4e6a8
akbarpn136 <akbarpn136@gmail.com>
2023-12-19 12:37:58 +0700

    Daftar command baru

--- aerofoil/main.py
+++ aerofoil/main.py
@@ -1,9 +1,14 @@
 import typer
 
 from aerofoil.commands.clean_image import cleanimg
+from aerofoil.commands.train_image import trainimg
 
 app = typer.Typer(help="Aplikasi CLI untuk keperluan pelatihan model CNN.")
 
 app.command(
     help="Fungsi untuk membersihkan images yang tidak ada dalam out.csv. Hasil disimpan dalam folder out (default)."
 )(cleanimg)
+
+app.command(
+    help="Fungsi untuk melatih model AI dengan CNN."
+)(trainimg)
