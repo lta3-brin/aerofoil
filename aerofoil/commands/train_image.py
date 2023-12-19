ab571fc3db74307dc978b348ac98fe7078671dda
akbarpn136 <akbarpn136@gmail.com>
2023-12-19 14:10:55 +0700

    Menambahkan argumen dalam command

--- aerofoil/commands/train_image.py
+++ aerofoil/commands/train_image.py
@@ -5,11 +5,43 @@ import tensorflow_datasets as tfds
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+
 def trainimg(
-    fname: Annotated[str, typer.Argument(
-        help="File csv yang diolah.")] = "out.csv",
-    re: Annotated[str, typer.Option(
-        help="Kolom Reynolds number dalam csv.")] = "re",
+    batchsize: Annotated[
+        int, typer.Argument(help="Jumlah grup dalam kumpulan dataset.")
+    ] = 100,
+    showcount: Annotated[bool, typer.Option(help="Tampilkan jumlah dataset?")] = False,
+    imgshowtrain: Annotated[
+        bool, typer.Option(help="Tampilkan gambar sampel train?")
+    ] = False,
+    imgshowtest: Annotated[
+        bool, typer.Option(help="Tampilkan gambar sampel train?")
+    ] = False,
 ):
-    ds = tfds.load("aerofoil_datasets", split="train", shuffle_files=True)
-    print(ds)
+    dstrain, infotrain = tfds.load(
+        "aerofoil_datasets",
+        split="train",
+        batch_size=batchsize,
+        with_info=True,
+        as_supervised=True,
+        shuffle_files=True,
+    )
+
+    dstest, infotest = tfds.load(
+        "aerofoil_datasets",
+        split="test",
+        batch_size=batchsize,
+        with_info=True,
+        as_supervised=True,
+        shuffle_files=True,
+    )
+
+    if showcount:
+        print(f"Jumlah datasets (Train) = {len(dstrain)}")
+        print(f"Jumlah datasets (Test) = {len(dstest)}")
+
+    if imgshowtrain:
+        tfds.show_examples(dstrain, infotrain, is_batched=True)
+
+    if imgshowtest:
+        tfds.show_examples(dstest, infotest, is_batched=True)
