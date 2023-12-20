5b5d2e26dee8f4316b55d8a058671dbe583a363e
akbarpn136 <akbarpn136@gmail.com>
2023-12-20 14:43:43 +0700

    Terapan awal pelatihan model

--- aerofoil/commands/train_image.py
+++ aerofoil/commands/train_image.py
@@ -1,16 +1,17 @@
-import os
 import typer
 import tensorflow as tf
 from typing import Annotated
 import tensorflow_datasets as tfds
 
 from aerofoil.helpers.image import normalize_img
+from aerofoil.helpers.conv3 import Aerofoil3BN2FC
 
 
 def trainimg(
+    epoch: Annotated[int, typer.Argument(help="Total pelatihan yang dilakukan.")] = 100,
     batchsize: Annotated[
-        int, typer.Argument(help="Jumlah grup dalam kumpulan dataset.")
-    ] = 100,
+        int, typer.Option(help="Jumlah grup dalam kumpulan dataset.")
+    ] = 128,
     showcount: Annotated[bool, typer.Option(help="Tampilkan jumlah dataset?")] = False,
     imgshowtrain: Annotated[
         bool, typer.Option(help="Tampilkan gambar sampel untuk latihan?")
@@ -18,8 +19,10 @@ def trainimg(
     imgshowvalid: Annotated[
         bool, typer.Option(help="Tampilkan gambar sampel untuk validasi?")
     ] = False,
+    lr: Annotated[
+        float, typer.Option(help="Learning rate yang dipilih untuk Adam optimizer.")
+    ] = 10e-4,
 ):
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
     (dstrain, dsvalid), info = tfds.load(
         "aerofoil_datasets",
         split=["train", "valid"],
@@ -47,3 +50,15 @@ def trainimg(
 
     if imgshowvalid:
         tfds.show_examples(dsvalid, info, is_batched=True)
+
+    image_shape = info.features["image"].shape
+    arch = Aerofoil3BN2FC(input_shape=image_shape)
+    model = arch.get_model()
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+        loss=tf.keras.losses.MeanSquaredError(),
+        metrics=tf.keras.metrics.R2Score(name="r2"),
+    )
+
+    model.fit(dstrain, epochs=epoch, validation_data=dsvalid)
