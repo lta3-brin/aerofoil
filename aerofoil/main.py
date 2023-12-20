550c8f89ffbf94338e432c8d8d5d9f6467907adb
akbarpn136 <akbarpn136@gmail.com>
2023-12-20 14:43:18 +0700

    Koreksi minor whitespace

--- aerofoil/main.py
+++ aerofoil/main.py
@@ -9,6 +9,4 @@ app.command(
     help="Fungsi untuk membersihkan images yang tidak ada dalam out.csv. Hasil disimpan dalam folder out (default)."
 )(cleanimg)
 
-app.command(
-    help="Fungsi untuk melatih model AI dengan CNN."
-)(trainimg)
+app.command(help="Fungsi untuk melatih model AI dengan CNN.")(trainimg)
