c43414b34a5b8aa5a597a7f22c443bcba000db00
akbarpn136 <akbarpn136@gmail.com>
2023-12-22 07:07:36 +0700

    Modifikasi arsitektur model

--- aerofoil/helpers/conv3.py
+++ aerofoil/helpers/conv3.py
@@ -9,33 +9,28 @@ class Aerofoil3BN2FC:
                 # Conv Layer 1
                 tf.keras.layers.Conv2D(
                     filters=16,
-                    kernel_size=(9, 9),
+                    kernel_size=(3, 3),
                     strides=(1, 1),
-                    padding="same",
                     activation="relu",
                     input_shape=self.input_shape,
                 ),
-                tf.keras.layers.BatchNormalization(),
                 tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
                 # Conv Layer 2
                 tf.keras.layers.Conv2D(
                     filters=32,
-                    kernel_size=(7, 7),
+                    kernel_size=(3, 3),
                     strides=(1, 1),
-                    padding="same",
                     activation="relu",
                 ),
-                tf.keras.layers.BatchNormalization(),
                 tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
                 # Conv Layer 3
                 tf.keras.layers.Conv2D(
                     filters=64,
                     kernel_size=(3, 3),
                     strides=(1, 1),
-                    padding="same",
+                    # padding="same",
                     activation="relu",
                 ),
-                tf.keras.layers.BatchNormalization(),
                 tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
                 # Fully connected Layer 1
                 tf.keras.layers.Flatten(),
